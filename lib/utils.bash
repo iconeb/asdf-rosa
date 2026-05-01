6d560157746b0b267e0e68fc0df8d2b771be22e0
Federico Nebiolo <iconeb@yahoo.it>
2026-05-02 00:07:35 +0200

    Fix to latest version

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for rosa.
 GH_REPO="https://github.com/openshift/rosa"
 TOOL_NAME="rosa"
 TOOL_TEST="rosa version"
@@ -14,7 +13,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if rosa is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -27,24 +25,44 @@ sort_versions() {
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		sed 's/^v//'
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if rosa has other means of determining installable versions.
 	list_github_tags
 }
 
+get_platform() {
+	local os arch
+
+	os="$(uname -s)"
+	arch="$(uname -m)"
+
+	case "$os" in
+	Darwin) os="Darwin" ;;
+	Linux) os="Linux" ;;
+	*) fail "Unsupported operating system: $os" ;;
+	esac
+
+	case "$arch" in
+	x86_64 | amd64) arch="x86_64" ;;
+	aarch64 | arm64) arch="arm64" ;;
+	i386 | i686) arch="i386" ;;
+	*) fail "Unsupported architecture: $arch" ;;
+	esac
+
+	echo "${os}_${arch}"
+}
+
 download_release() {
-	local version filename url
+	local version filename url platform
 	version="$1"
 	filename="$2"
+	platform="$(get_platform)"
 
-	# TODO: Adapt the release URL convention for rosa
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="${GH_REPO}/releases/download/v${version}/rosa_${platform}.tar.gz"
 
-	echo "* Downloading $TOOL_NAME release $version..."
+	echo "* Downloading $TOOL_NAME release $version for $platform..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
@@ -59,9 +77,9 @@ install_version() {
 
 	(
 		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		cp "$ASDF_DOWNLOAD_PATH/rosa" "$install_path/rosa"
+		chmod +x "$install_path/rosa"
 
-		# TODO: Assert rosa executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
